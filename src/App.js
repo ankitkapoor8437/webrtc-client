1058be64aeb5ff3238a7fe6dd2ad4f409b6e3a9f
Ankit Kapoor <ankitkapoor8437@gmail.com>
2023-09-14 11:46:37 +0530

    Socket implimentation using context.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
+import {SocketProvider} from "./providers/Socket";
 import '../src/App.css'
 
 function App() {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-      </Routes>
+      <SocketProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </SocketProvider>
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,33 @@
+import { useState } from 'react'
 import React from 'react'
+import { useSocket } from '../providers/Socket'
+import { useNavigate } from 'react-router-dom'
+import { useEffect } from 'react'
 
 export const Home = () => {
+    const navigate = useNavigate();
+    const { socket } = useSocket();
+    const [email, setEmail] = useState();
+    const [roomId, setRoomId] = useState();
+
+    const handleRoomJoined = ({roomId}) => {
+        navigate(`/room/${roomId}`)
+    }
+
+    useEffect(() => {
+        socket.on('joined-room', handleRoomJoined);
+    }, [socket]);
+
+    const handleJoinRoom = () => {
+        socket.emit("join-room", { emailId: email, roomId })
+    }
+
     return (
         <div className='homepage-container'>
             <div className='input-container'>
-                <input type="email" placeholder='Enter your email here!' />
-                <input type="text" placeholder='Enter Room code' />
-                <button>Enter Room</button>
+                <input value={email} onChange={e => setEmail(e.target.value)} type="email" placeholder='Enter your email here!' />
+                <input value={roomId} onChange={e => setRoomId(e.target.value)} type="text" placeholder='Enter Room code' />
+                <button onClick={() => handleJoinRoom()}>Enter Room</button>
             </div>
         </div>
     )
